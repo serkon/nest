36f481f55fb1c9fbb6a2ca38ea31a8e1ab9550ec
Serkan Konakcı <serkan.konakci@timusnetworks.com>
2025-03-22 17:09:30 +0300

    refactor: add enums file

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,28 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { exec } from 'child_process';
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! Test02';
-  }
-
-  handleWebhook(payload: any) {
-    if (payload.ref === 'refs/heads/master' || payload.ref === 'refs/heads/main') {
-      console.log('GitHub push detected. Updating...');
-      exec(
-        'git pull origin "$(echo $payload.ref | sed \'s/refs\\/heads\\//\')" && npm install && pm2 restart dirdir-dev',
-        (err, stdout, stderr) => {
-          if (err) {
-            console.error(`exec error: ${err}`);
-            return;
-          }
-          console.log(`stdout: ${stdout}`);
-          console.error(`stderr: ${stderr}`);
-        },
-      );
-    } else {
-      console.log('Ignoring push to non-master/main branch. 2');
-    }
+    return 'Hello World! Test03';
   }
 }
